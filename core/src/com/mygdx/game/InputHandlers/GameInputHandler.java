15b48a324d3451b948ac2aa7582631e601bb0dad
Jonathan <jonfpoole@gmail.com>
2015-06-14 14:09:13 +0300

    Added skip turn to game input handler on key 't'

--- core/src/com/mygdx/game/InputHandlers/GameInputHandler.java
+++ core/src/com/mygdx/game/InputHandlers/GameInputHandler.java
@@ -3,12 +3,17 @@ package com.mygdx.game.InputHandlers;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputAdapter;
+import com.mygdx.game.GameHandler;
 
 public class GameInputHandler extends InputAdapter {
     @Override
-    public boolean keyDown(int keycode) {
-        if (keycode == Input.Keys.ESCAPE) {
+    public boolean keyDown(int keyCode) {
+        if (keyCode == Input.Keys.ESCAPE) {
             Gdx.app.exit();
+            return true;
+        }
+        else if (keyCode == Input.Keys.T){
+            GameHandler.stepTurn();
             return true;
         }
 
--- core/src/com/mygdx/game/InputHandlers/PlayerInputHandler.java
+++ core/src/com/mygdx/game/InputHandlers/PlayerInputHandler.java
@@ -33,8 +33,8 @@ public class PlayerInputHandler extends InputAdapter {
         }
         else if (keyCode == Input.Keys.SPACE){
             pickUpItem();
+            return true;
         }
-        
 
         return checkDpadDown(keyCode);
     }
