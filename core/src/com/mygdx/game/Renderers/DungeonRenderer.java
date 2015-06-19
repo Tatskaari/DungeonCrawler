df45b3846592245584f0753244c4ef86d5953c79
Jonathan <jonfpoole@gmail.com>
2015-06-19 23:15:55 +0100

    Fixed some bugs with the changes to the dungeon renderer

--- core/src/com/mygdx/game/Renderers/DungeonRenderer.java
+++ core/src/com/mygdx/game/Renderers/DungeonRenderer.java
@@ -2,17 +2,11 @@ package com.mygdx.game.Renderers;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.mygdx.game.Characters.NonPlayerCharacterEntity;
 import com.mygdx.game.Dungeon.Dungeon;
 import com.mygdx.game.Dungeon.DungeonTile;
 import com.mygdx.game.Characters.CharacterEntity;
-import com.mygdx.game.Inventory.Inventory;
 import com.mygdx.game.Inventory.ItemTypes.InventoryItem;
 import com.mygdx.game.ResourceLoader;
-
-import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class DungeonRenderer extends Renderer{
     private final Dungeon dungeon;
@@ -28,60 +22,68 @@ public class DungeonRenderer extends Renderer{
         tileSize = ResourceLoader.getTileSize();
     }
 
-    private void render(float delta, SpriteBatch batch, TileRenderer tileRenderer){
+    private void renderTiles(float delta, SpriteBatch batch, TileRenderer tileRenderer){
         for (int i = -1; i < dungeon.getMapWidth()+1; i++){
             for (int j = -1; j < dungeon.getMapHeight()+1; j++){
                 DungeonTile tile = dungeon.getDungeonTile(i, j);
                 tileRenderer.renderTile(tile, batch, i, j);
             }
         }
+    }
 
+    @Override
+    public void render(float delta, SpriteBatch batch){
+        renderTiles(delta, batch, this::renderTile);
         for (CharacterEntity characterEntity : dungeon.monsters){
             characterEntity.getRenderer().render(delta,batch);
         }
     }
 
-    public void render(float delta, SpriteBatch batch){
-        render(delta, batch, this::renderTile);
-    }
-
     @Override
     public void devRender(float delta, SpriteBatch batch){
-        render(delta, batch, this::devRenderTile);
+        renderTiles(delta, batch, this::devRenderTile);
+        for (CharacterEntity characterEntity : Dungeon.getActiveDungeon().monsters){
+            characterEntity.getRenderer().devRender(delta, batch);
+        }
     }
 
     public void mapRender(float delta, SpriteBatch batch) {
-        render(delta, batch, this::mapRenderTile);
+        renderTiles(delta, batch, this::mapRenderTile);
     }
 
-    void renderTile(DungeonTile tile, SpriteBatch batch, int x, int y) {
+    private void renderTile(DungeonTile tile, SpriteBatch batch, int x, int y) {
         if (!tile.isEmpty()){
             float colourVal = tile.getVisibilityLevel();
             batch.setColor(colourVal, colourVal, colourVal, colourVal);
-            batch.draw(tile.getTileTexture(), x * tileSize, y * tileSize);
+            drawDungeonTile(tile, batch, x, y);
             if (tile.isVisible()){
-                for (InventoryItem item : tile.getItems()){
-                    TextureRegion textureRegion = ResourceLoader.itemTextureAtlas.findRegion(item.getTextureName());
-                    batch.draw(textureRegion, x * tileSize, y * tileSize);
-                }
+                drawTileItems(tile, batch, x, y);
             }
             batch.setColor(1,1,1,1);
         }
     }
 
-    void devRenderTile(DungeonTile tile, SpriteBatch batch, int x, int y) {
+    private void devRenderTile(DungeonTile tile, SpriteBatch batch, int x, int y) {
         if (!tile.isEmpty()){
-            batch.draw(tile.getTileTexture(), x * ResourceLoader.getTileSize(), y * ResourceLoader.getTileSize());
-            for (InventoryItem item : tile.getItems()){
-                TextureRegion textureRegion = ResourceLoader.itemTextureAtlas.findRegion(item.getTextureName());
-                batch.draw(textureRegion, x * tileSize, y * tileSize);
-            }
+            drawDungeonTile(tile, batch, x, y);
+            drawTileItems(tile, batch, x, y);
         }
     }
 
-    void mapRenderTile(DungeonTile tile, SpriteBatch batch, int x, int y) {
+    private void mapRenderTile(DungeonTile tile, SpriteBatch batch, int x, int y) {
         if (!tile.isEmpty() && tile.isDiscovered()){
-            batch.draw(tile.getTileTexture(), x * ResourceLoader.getTileSize(), y * ResourceLoader.getTileSize());
+            drawDungeonTile(tile, batch, x, y);
+        }
+    }
+
+    private void drawDungeonTile(DungeonTile tile, SpriteBatch batch,  int x, int y){
+        batch.draw(tile.getTileTexture(), x * ResourceLoader.getTileSize(), y * ResourceLoader.getTileSize());
+    }
+
+    private void drawTileItems(DungeonTile tile, SpriteBatch batch, int x, int y){
+        for (InventoryItem item : tile.getItems()){
+            TextureRegion textureRegion = ResourceLoader.itemTextureAtlas.findRegion(item.getTextureName());
+            batch.draw(textureRegion, x * tileSize, y * tileSize);
         }
     }
 
